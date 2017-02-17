325f384f0a0facdd160ac80898379a3eced0bd97
josmet52 <joseph.metrailler@bluewin.ch>
2017-02-17 11:39:31 -0400

    v1.0

--- led.py
+++ led.py
@@ -1,37 +1,42 @@
-﻿import RPi.GPIO as GPIO 
+"""
+This small program causes a led to flash at a given frequency
+    GPIO channel = 12
+    Frequency = 5 Hz
+"""
+
+import RPi.GPIO as GPIO 
 import time
 
-# Constantes
+# Constants
 GPIO_channel_number = 12 # channel used --> setmode = GPIO.BCM
-LED_status=False
 LED_off = False
+LED_status = LED_off
 
-# frequence de clignotement de la LED en Hertz (nombre de flashs par seconde)
-LED_frequence = 2
-# temps de pause entre chaque changement d'état de la LED
-# utiliser 1.0 (et pas 1) pour que la variable LED_period soit un float
-LED_half_period = 1.0 / LED_frequence / 2 
+# LED blink frequency in Hertz (number of flashs every second)
+LED_blink_frequency = 5
+# pause time between each change in the state of the LED
+LED_half_period = 1.0 / LED_blink_frequency / 2 
 
 # Init GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-# mettre la sortie GPIO_output en mode sortie
+# set GPIO_channel_number in output mode
 GPIO.setup(GPIO_channel_number,GPIO.OUT)
-# initialiser la LED a OFF
-GPIO.output(GPIO_channel_number,LED_off)
+GPIO.output(GPIO_channel_number,LED_status) # init the LED to OFF
 
 try:
     print("runing, CTRL-C to stop")
-    # boucle sans fin -> CTRL-C pour terminer
+    print("blink frequency = " + str(LED_blink_frequency))
+    # endless loop -> CTRL-C to stop
     while True:
-        # changer le status de la variable LED_status si 1 -> 0 si 0 -> 1
+        # change the status of the LED (1 -> 0 si 0 -> 1)
         LED_status = not LED_status
-        # apliquer l'etat à la sortie
+        # apply state to output
         GPIO.output(GPIO_channel_number,LED_status)
-        # faire une pause pour que la fréquence de clignotment soir celle choisie
+        # apply the pause time to respect the frequency
         time.sleep(LED_half_period)
         
 except KeyboardInterrupt:
     print("bye")
-    GPIO.output(GPIO_channel_number,LED_off)
+    GPIO.output(GPIO_channel_number,LED_off) # switch the LED OFF
     pass
